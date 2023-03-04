3ca4b83ee7db9029ca345acda3cfa727ae4d096b
Sergio Arbeo <serabe@gmail.com>
2023-03-04 01:14:12 +0100

    First try at fixing #22

--- test_app/mix.exs
+++ test_app/mix.exs
@@ -38,7 +38,7 @@ defmodule TestApp.MixProject do
     [
       {:live_isolated_component, path: "../."},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:phoenix, "~> 1.6.7"},
+      {:phoenix, "~> #{System.get_env("PHOENIX_VERSION", "1.6.7")}"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
