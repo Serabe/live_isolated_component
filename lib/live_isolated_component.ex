1c2f19de8e9c90bdb90a2cbbab6039569b0e5bf8
Sergio Arbeo <serabe@gmail.com>
2024-12-23 00:46:38 +0100

    Create ViewUtils and let the mock view be passed in. (#42)
    * Extract mount to ViewUtils
    
    * Fix compilation
    
    * Add simple API for creating mock views
    
    * Move dialyzer out of Elixir CI
    
    * Document LiveIsolatedComponent.View`
    
    * Improve docs

--- lib/live_isolated_component.ex
+++ lib/live_isolated_component.ex
@@ -46,6 +46,7 @@ defmodule LiveIsolatedComponent do
     - `:assigns` accepts a map of assigns for the component.
     - `:handle_event` accepts a handler for the `handle_event` callback in the LiveView.
     - `:handle_info` accepts a handler for the `handle_info` callback in the LiveView.
+    - `:mock_view` accepts a Phoenix.LiveView to use as mock view. Please, refer to `LiveIsolatedComponent.View` for more info on how to customise these views.
     - `:on_mount` accepts a list of either modules or tuples `{Module, parameter}`. See `Phoenix.LiveView.on_mount/1` for more info on the parameters.
     - `:slots` accepts different slot descriptors.
 
@@ -84,7 +85,9 @@ defmodule LiveIsolatedComponent do
           }
         end)
 
-      live_isolated(build_conn(), LiveIsolatedComponent.View,
+      live_isolated(
+        build_conn(),
+        Keyword.get(opts, :mock_view, LiveIsolatedComponent.View.LiveView),
         session: %{
           unquote(LiveIsolatedComponent.MessageNames.store_agent_key()) => store_agent
         }
